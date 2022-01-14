666bcd50fd2b90ca30255a908091f0b618cc01d5
Qsara00 <quaiato22@gmail.com>
2022-01-14 12:31:16 +0100

    add

---
+++ client/src/components/View.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 class="flex flex-col items-center m-4 text-lg font-medium">DETTAGLI</h1>
+        {{}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:"View",
+    data() {
+      return {
+        offer: {}
+      }
+    },
+    async mounted() {
+      let platformId = this.$route.params.id;
+      let response = await this.$axios.get('http://localhost:8000/api/platforms' + platformId);
+      this.offer = response.data;
+    }
+}
+</script>
+
+<style>
+
+</style>
